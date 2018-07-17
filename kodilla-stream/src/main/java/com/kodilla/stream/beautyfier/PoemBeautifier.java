5a04d8c5b7328d30eaaca63f101fa2e95daee156
Arkadiusz-Kotela <arkadiusz.kotela@gmail.com>
2018-07-17 18:56:44 +0300

    7.1. Wstęp do programowania funkcyjnego, czyli wyrażenia lambda - small update

--- kodilla-stream/src/main/java/com/kodilla/stream/beautyfier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautyfier/PoemBeautifier.java
@@ -3,6 +3,6 @@ package com.kodilla.stream.beautyfier;
 public class PoemBeautifier {
     public void beautify(String text, PoemDecorator poemDecorator) {
         String decoratedText = poemDecorator.decorate(text);
-        System.out.println("Decorated text: " + decoratedText);
+        System.out.println("Original text: " + text + " -> Decorated text: " + decoratedText);
     }
 }
