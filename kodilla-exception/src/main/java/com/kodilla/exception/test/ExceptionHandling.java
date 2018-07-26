138af6c812e67b7a91370fd5a0d21a2f8f568fdd
Arkadiusz-Kotela <arkadiusz.kotela@gmail.com>
2018-07-26 12:42:57 +0300

    Zadanie: Obsługa wyjątku - poprawka

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,16 @@
 package com.kodilla.exception.test;
 
-public class ExceptionHandling extends Exception {
+public class ExceptionHandling {
+
+    public ExceptionHandling() {
+
+        SecondChallenge sc = new SecondChallenge();
+        try {
+            sc.probablyIWillThrowException(1.5, 1.5);
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e);
+        } finally {
+            System.out.println("End of the programme.");
+        }
+    }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
@@ -2,14 +2,9 @@ package com.kodilla.exception.test;
 
 public class SecondChallenge {
 
-    public String probablyIWillThrowException(double x, double y) {
-
-        try {
-            if (x >= 2 || x < 1 || y == 1.5) {
-                throw new ExceptionHandling();
-            }
-        } catch (ExceptionHandling e) {
-            System.out.println("Huston we have a problem! Error: " + e);
+    public String probablyIWillThrowException(double x, double y) throws Exception {
+        if (x >= 2 || x < 1 || y == 1.5) {
+            throw new Exception();
         }
         return "Done!";
     }
