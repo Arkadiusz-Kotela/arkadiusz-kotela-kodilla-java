de214f5276d6aedfe4b80e45d787bdbbdcebce1e
Arkadiusz-Kotela <arkadiusz.kotela@gmail.com>
2018-08-01 18:46:52 +0300

    9.1. Idea czystego kodu

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,44 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MovieStore {
+
+    private static Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+
+    public static void main(String[] args) {
+
+        StringBuilder sc = new StringBuilder();
+
+        String moviesString = getMovies().entrySet().stream()
+                .flatMap(e -> e.getValue().stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.println(moviesString);
+    }
+
+}
