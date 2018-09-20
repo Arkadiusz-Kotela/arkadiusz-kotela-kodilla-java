487122e264b2f223ff357f9c0fc85099629011f9
Arkadiusz-Kotela <arkadiusz.kotela@gmail.com>
2018-09-20 21:46:44 +0300

    General update

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KodillaHibernateApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KodillaHibernateApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaHibernateApplication.class, args);
+	}
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class KodillaHibernateApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
